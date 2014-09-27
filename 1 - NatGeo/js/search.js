2e8923799730568a7f21fc8d0b5a9652b2f1a550
EgorKluch <EgorKluch@gmail.com>
2014-09-27 21:33:52 +0700

    Search input works (with select category menu)

--- 1 - NatGeo/js/search.js
+++ 1 - NatGeo/js/search.js
@@ -7,24 +7,67 @@
 
 $.fn.searchInput = function () {
   var $labelContainer = $('.search_category_labelContainer', this);
-  var toggleClass;
+  var $categoryContainer = $('#searchCategories', this);
+  var $categoryLabel = $('.search_category_label');
+  var $categoryItems = $('.search_category_listItem', this);
+  var $selectAllCategory = $('.search_category_selectAll', this);
+  var clickedHover = false;
 
-  var $searchCategories = $('#searchCategories', this);
-  $labelContainer.click(function () { toggleClass = true; });
+  var isSelectAll = function () {
+    return !$categoryItems.not('.selected').length;
+  };
+
+  var setCategoryLabel = function () {
+    if (isSelectAll() || $selectAllCategory.hasClass('checked')) {
+      $categoryLabel.html('All categories');
+      $categoryLabel.attr('title', 'All categories');
+      return;
+    }
+
+    var $selectedCategories = $categoryItems.filter('.selected');
+    if (!$selectedCategories.length) {
+      $categoryLabel.html('All categories');
+      $categoryLabel.attr('title', 'All categories');
+      return;
+    }
+
+    var label = '';
+    $selectedCategories.each(function (index, el) {
+      if (label.length) {
+        label += ', ';
+      }
+      label += $(el).html();
+    });
+    $categoryLabel.attr('title', label);
+    var postfix = label.length > 13 ? ' ..' : '';
+    label = label.substr(0, 13) + postfix;
+    $categoryLabel.html(label);
+  };
+
+  $labelContainer.click(function () {
+    $labelContainer.toggleClass('active');
+    $categoryContainer.toggle();
+    clickedHover = true;
+  });
+  $categoryContainer.click(function () {
+    clickedHover = true;
+  });
+  $categoryItems.click(function () {
+    $(this).toggleClass('selected');
+    setCategoryLabel();
+  });
+  $selectAllCategory.click(function () {
+    $(this).toggleClass('checked');
+    setCategoryLabel();
+  });
 
   $(document).click(function () {
-    if (toggleClass) {
-      $labelContainer.toggleClass('active');
-      if ($labelContainer.hasClass('active')) {
-        $searchCategories.show();
-      } else {
-        $searchCategories.hide();
-      }
-      toggleClass = false;
-    } else {
-      $labelContainer.removeClass('active');
-      $searchCategories.hide();
+    if (clickedHover) {
+      clickedHover = false;
+      return;
     }
+    $labelContainer.removeClass('active');
+    $categoryContainer.hide();
   });
 
   return this;
