57330744cad6b1a70a0db35c062fcc26ca1b4da2
EgorKluch <EgorKluch@gmail.com>
2014-09-27 09:45:19 +0700

    Re-layout header (logo and search input)

--- 1 - NatGeo/js/search.js
+++ 1 - NatGeo/js/search.js
@@ -9,20 +9,27 @@ $.fn.searchInput = function () {
   var $labelContainer = $('.search_category_labelContainer', this);
   var toggleClass;
 
+  var $searchCategories = $('#searchCategories', this);
   $labelContainer.click(function () { toggleClass = true; });
 
   $(document).click(function () {
     if (toggleClass) {
       $labelContainer.toggleClass('active');
+      if ($labelContainer.hasClass('active')) {
+        $searchCategories.show();
+      } else {
+        $searchCategories.hide();
+      }
       toggleClass = false;
-      return;
+    } else {
+      $labelContainer.removeClass('active');
+      $searchCategories.hide();
     }
-    $labelContainer.removeClass('active');
   });
 
   return this;
 };
 
 $(document).ready(function () {
-  $('#headerSearch').searchInput();
+  $('#search').searchInput();
 });
